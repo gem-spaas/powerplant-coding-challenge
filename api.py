8104ee5d9e6a0feb02f7e05042adb130fb8155f4
Woody1203 <wudi04009@gmail.com>
2021-03-21 14:11:48 +0100

    update
    rest api with fixed endpoint and port
    requirement.txt
    msg logger

--- api.py
+++ api.py
@@ -1,26 +1,31 @@
 from flask import Blueprint, request, current_app, jsonify, Response
 import json, flask
-from test import Network, solver
-import pandas as pd
-
+from test import solver
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
 @app.route('/', methods=['GET'])
-def home():
-    #data = flask.request.form
-    #print(data)
-    print(solver())
-    return solver()
+def note():
+    return 'powerplant-coding-challenge-demo'
 
 @app.route('/productionplan', methods=['POST'])
 def productionplan():
-    data = flask.request.json
+    # payload = flask.request.json
+    current_app.logger.info(f"Solver service requested")
+    if request.json is None:
+        current_app.logger.warning("No json file provided")
+        return jsonify(message="No json file provided"), 400
+    try:
     #regex to verify data here
-    LB1 = LoadBalancer(False, data)
-    result = LB1.CalcCost()
-
-    return result
+        response = solver(flask.request.json)
+        current_app.logger.warning("Problem solved")
+        return jsonify(response), 200
+    except IndexError as e:
+        current_app.logger.error("Unsolvable request")
+        return jsonify(message=f"Unsolvable request"), 202
+    except Exception as e:
+        current_app.logger.error("Solver service just crashed")
+    return jsonify(message=f"Unfortunately, solver service just crashed"), 500
 
 app.run('localhost', port=8888)
---
+++ url_test.py
@@ -0,0 +1,14 @@
+import requests
+import json
+
+url = 'http://localhost:8888/productionplan'
+path = "C:\\Users\\15754\\Desktop\\homework\\payload1.json"
+
+response = requests.post(url, json = path)
+
+# print(json_object.content)
+
+
+print("Status code: ", response.status_code)
+print("Printing Entire Post Request")
+print(response.json())

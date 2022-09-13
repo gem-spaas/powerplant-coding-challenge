946d4dc5db66a9b2a90bb8041a3d2e7007588351
Franz014 <moka78@live.be>
2022-09-13 13:01:31 +0200

    arch_02

--- code/powerplant-coding-challenge/powerplant-coding-challenge/IPhysicFactor.cs
+++ code/powerplant-coding-challenge/powerplant-coding-challenge/IPhysicFactor.cs
@@ -5,6 +5,14 @@
     /// </summary>
     public interface IPhysicFactor
     {
+        // -------------- Methods --------------
+        /// <summary>
+        /// This method is used to compute the final power of the productor
+        /// </summary>
+        /// <param name="powerInput">This is the input power of the productor.</param>
+        /// <returns>The output power of the productor.</returns>
+        Double ComputePower (Double powerInput);
+
         // -------------- Properties --------------
         /// <summary>
         /// This is the current physic value.
---
+++ code/powerplant-coding-challenge/powerplant-coding-challenge/WindFactor.cs
@@ -0,0 +1,35 @@
+﻿namespace powerplant_coding_challenge
+{
+    /// <summary>
+    /// This is the class used to add a wind factor to a productor.
+    /// </summary>
+    public class WindFactor : IPhysicFactor
+    {
+
+        // -------------- Constructors --------------
+        /// <summary>
+        /// This is the main constructor of the wind factor object.
+        /// </summary>
+        /// <param name="windValue">This is the wind value.</param>
+        public WindFactor (Double windValue)
+        {
+            this.PhysicValue = windValue;
+        }
+
+        // -------------- Methods --------------
+
+        public double ComputePower(double powerInput)
+        {
+            return powerInput * this.PhysicValue;
+        }
+
+
+        // -------------- Methods --------------
+
+        public Double PhysicValue
+        {
+            get;
+            set;
+        } = 0;
+    }
+}

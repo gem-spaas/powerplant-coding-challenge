b598b397d8163e3f21fe930fd6be09f7b9244c50
Franz014 <moka78@live.be>
2022-09-13 12:25:05 +0200

    add-code

---
+++ code/powerplant-coding-challenge/powerplant-coding-challenge/Program.cs
@@ -0,0 +1,61 @@
+//-----------------------------------
+// Imports
+//-----------------------------------
+
+using Newtonsoft.Json.Linq;
+using powerplant_coding_challenge;
+
+//-----------------------------------
+// Fields
+//-----------------------------------
+
+/// <summary>
+/// This is the builder that allows to instanciate the application launhcer.
+/// </summary>
+var builder = WebApplication.CreateBuilder(args);
+
+/// <summary>
+/// This is the application.
+/// </summary>
+var app = builder.Build();
+
+/// <summary>
+/// This is the input file used as an input.
+/// </summary>
+const String INPUT_FILE_NAME = "payload1.json";
+
+/// <summary>
+/// This is the output file used as an output.
+/// </summary>
+const String OUTPUT_FILE_NAME = "example_response.json";
+
+//-----------------------------------
+// Functions
+//-----------------------------------
+
+String ProductionplanMethod(String fileName)
+{
+}
+
+//-----------------------------------
+// Routes
+//-----------------------------------
+
+/// <summary>
+/// This is the required first route.
+/// </summary>
+app.MapGet("/productionplan", () => ProductionplanMethod("payload1.json"));
+
+/// <summary>
+/// This is the route in order to know that everything work.
+/// </summary>
+app.MapGet("/", () => "Hello World !");
+
+//-----------------------------------
+// Main
+//-----------------------------------
+
+/// <summary>
+/// This is the launching of the application.
+/// </summary>
+app.Run();

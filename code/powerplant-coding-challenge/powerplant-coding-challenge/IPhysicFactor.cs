a42195ff88a988a364b305b503bc460cbc0f6c6e
Franz014 <moka78@live.be>
2022-09-13 12:56:35 +0200

    arch_01

---
+++ code/powerplant-coding-challenge/powerplant-coding-challenge/IPhysicFactor.cs
@@ -0,0 +1,18 @@
+﻿namespace powerplant_coding_challenge
+{
+    /// <summary>
+    /// This interface is used to modify the output of a productor thanks to a physic factor : wind, solar, ...
+    /// </summary>
+    public interface IPhysicFactor
+    {
+        // -------------- Properties --------------
+        /// <summary>
+        /// This is the current physic value.
+        /// </summary>
+        Double PhysicValue
+        {
+            get;
+            set;
+        }
+    }
+}

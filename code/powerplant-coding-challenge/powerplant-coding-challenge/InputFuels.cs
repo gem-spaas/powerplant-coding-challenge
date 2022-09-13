7858e83d298e4f5984223743df69a4fbf078e5ed
Franz014 <moka78@live.be>
2022-09-13 13:31:59 +0200

    arch_07

---
+++ code/powerplant-coding-challenge/powerplant-coding-challenge/InputFuels.cs
@@ -0,0 +1,42 @@
+﻿namespace powerplant_coding_challenge
+{
+
+    /// <summary>
+    /// This is the class used to store the data of the fuel imported.
+    /// </summary>
+    public class InputFuels
+    {
+        /// <summary>
+        /// This is the price rate of the gas.
+        /// </summary>
+        public Double GasEuroMWH
+        {
+            get;
+        }
+
+        /// <summary>
+        /// This is the price rate of the kerosine.
+        /// </summary>
+        public Double KersosinEuroMWH
+        {
+            get;
+        }
+
+        /// <summary>
+        /// This is the price rate of the reject of CO2.
+        /// </summary>
+        public Double CO2EuroTon
+        {
+            get;
+        }
+        
+        /// <summary>
+        /// This is the percentage of the wind.
+        /// </summary>
+        public Double WindPercent
+        {
+            get;
+        }
+
+    }
+}

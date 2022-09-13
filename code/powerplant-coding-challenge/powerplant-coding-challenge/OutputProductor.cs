e4f8f6c827861c8c4a47094e06ea78f68b0e39dc
Franz014 <moka78@live.be>
2022-09-13 14:51:16 +0200

    arch_07

---
+++ code/powerplant-coding-challenge/powerplant-coding-challenge/OutputProductor.cs
@@ -0,0 +1,35 @@
+﻿namespace powerplant_coding_challenge
+{
+    /// <summary>
+    /// This is the class that holds the output data of the productor.
+    /// </summary>
+    public class OutputProductor
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="power"></param>
+        public OutputProductor (String name, Double power)
+        {
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public String name
+        {
+            get;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public Double p
+        {
+            get;
+        }
+
+    }
+}

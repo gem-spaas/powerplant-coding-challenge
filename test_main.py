a96783f81cb9dbebb687e0fe7d9843baa4f55fd7
JeyPay <b32346@gmail.com>
2023-04-14 17:46:02 +0200

    Added endpoint test

---
+++ test_main.py
@@ -0,0 +1,34 @@
+import json
+from fastapi.testclient import TestClient
+
+from main import app
+
+client = TestClient(app)
+
+def load_fixture_payload(path: str):
+    with open(f'fixtures/payloads/{path}.json') as f:
+        payload = json.loads(''.join(f.readlines()))
+
+    with open(f'fixtures/responses/response_{path}.json') as f:
+        expected_response = json.loads(''.join(f.readlines()))
+
+    return (payload, expected_response)
+
+def test_productionplant():
+    payload, expected_response = load_fixture_payload("payload1")
+    response = client.post("/productionplant", json=payload)
+
+    assert response.status_code == 200
+    assert json.loads(response.json()) == expected_response
+
+    payload, expected_response = load_fixture_payload("payload2")
+    response = client.post("/productionplant", json=payload)
+
+    assert response.status_code == 200
+    assert json.loads(response.json()) == expected_response
+
+    payload, expected_response = load_fixture_payload("payload3")
+    response = client.post("/productionplant", json=payload)
+
+    assert response.status_code == 200
+    assert json.loads(response.json()) == expected_response

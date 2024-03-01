8a3c943a283c73d3e0022b2d9003f9997a61d0a1
João Paixão <joao00paixao@gmail.com>
2024-03-01 19:55:36 +0100

    src: adding endpoints

---
+++ GlobalEnergyManagement/GlobalEnergyManagement.Application/Endpoints/PowerPlantEndpoints.cs
@@ -0,0 +1,23 @@
+﻿namespace GlobalEnergyManagement.Application.Endpoints;
+
+public static class PowerPlantEndpoints
+{
+    public static void MapPowerPlantEndpoints(this IEndpointRouteBuilder group)
+    {
+        group.MapPost("/", CalculateProductionPlan);
+
+    }
+
+    private static async Task<IResult> CalculateProductionPlan()
+    {
+        try
+        {
+
+            return Results.Ok();
+        }
+        catch
+        {
+            return Results.Problem("An error happened while calculating production plan.");
+        }
+    }
+}
